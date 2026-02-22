a850761a779e44f76328737a7a9b5d8d1a80b6cd
Khairul <khairul.web1@gmail.com>
2026-02-22 15:09:44 +0600

    format parcel

--- src/parcels/parcels.controller.ts
+++ src/parcels/parcels.controller.ts
@@ -1,12 +1,32 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ParcelsService } from './parcels.service';
 
+interface TParcel {
+  delivery_instruction: string;
+  pickup_instruction: string;
+  receiver_address: string;
+  receiver_center: string;
+  receiver_contact: string;
+  receiver_name: string;
+  receiver_region: string;
+  sender_address: string;
+  sender_center: string;
+  sender_contact: string;
+  sender_name: string;
+  sender_region: string;
+  title: string;
+  type: string;
+  weight: string;
+  cost: number;
+  email: string;
+}
+
 @Controller('parcels')
 export class ParcelsController {
   constructor(private readonly parcelsService: ParcelsService) {}
 
   @Post('sendParcel')
-  sendParcel(@Body() body: any) {
+  sendParcel(@Body() body: TParcel) {
     return this.parcelsService.sendParcel(body);
   }
 }
--- src/parcels/parcels.service.ts
+++ src/parcels/parcels.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+interface TParcel {
+  delivery_instruction: string;
+  pickup_instruction: string;
+  receiver_address: string;
+  receiver_center: string;
+  receiver_contact: string;
+  receiver_name: string;
+  receiver_region: string;
+  sender_address: string;
+  sender_center: string;
+  sender_contact: string;
+  sender_name: string;
+  sender_region: string;
+  title: string;
+  type: string;
+  weight: string;
+  cost: number;
+  email: string;
+}
+
 @Injectable()
 export class ParcelsService {
-    constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
-    async sendParcel(body: any) {
-        return await this.prisma.parcel.create({
-            data: body,
-        });
-    }
+  async sendParcel(body: TParcel) {
+    return await this.prisma.parcel.create({
+      data: body,
+    });
+  }
 }
